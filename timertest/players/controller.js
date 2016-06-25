c363c6e4e5beac9635c6a159b3272c4de5bf3916
tbeenpwn <lukasroettgers@gmail.com>
2016-06-25 21:11:47 +0200

    Design Changes ver4

--- timertest/players/controller.js
+++ timertest/players/controller.js
@@ -6,28 +6,32 @@ helloApp.controller("CompanyCtrl", function($scope) {
             'name': 'Peter Meier',
             'paid': false,
             'staff': false,
-            'seat': '1-3',
+            'table': '1',
+            'seat': '3',
             'out': '22:00'
         },
         {
             'name': 'Aeter Deier',
             'paid': true,
             'staff': true,
-            'seat': '2-5',
+            'table': '1',
+            'seat': '3',
             'out': '22:00'
         },
         {
             'name': 'Deter Ceier',
             'paid': false,
             'staff': true,
-            'seat': '2-4',
+            'table': '1',
+            'seat': '3',
             'out': '22:00'
         },
         {
             'name': 'Beter Aeier',
             'paid': true,
             'staff': true,
-            'seat': '1-2',
+            'table': '1',
+            'seat': '3',
             'out': '22:00'
         },
     ];
@@ -37,22 +41,35 @@ helloApp.controller("CompanyCtrl", function($scope) {
         $scope.name = '';
         $scope.paid = false;
         $scope.staff = false;
+        $scope.table = '';
         $scope.seat = '';
         $scope.out = '';
+
+    };
+
+    $scope.assignSeats = function(){
+        $scope.companies[1].seat = 25;
     };
 
     $scope.$watch('companies', function(companies){
         $scope.count = 0;
         $scope.paidcount = 0;
+        localStorage.paidCount = 0;
+        localStorage.playerCount = companies.length;
+        localStorage.staffCount = 0;
+
         angular.forEach(companies, function(company){
             if(company.staff==true){
                 $scope.count += 1;
+                 localStorage.staffCount = $scope.count;
             }
             if(company.paid==true){
                 $scope.paidcount += 1;
+                localStorage.paidCount = $scope.paidcount;
             }
         })
     }, true);
+
 
 
     $scope.removeRow = function(name){
